62dbf9e6ffc61c6341516bc1412d8eabee30e552
kishorkc <kishor8007kc@gmail.com>
2021-12-30 18:51:13 +0545

    add order lottie

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:explore_lottie_flutter/pages/homepages.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,44 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'Lottie Flutter',
       theme: ThemeData(primarySwatch: Colors.indigo),
       home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    _controller = AnimationController(vsync: this);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child:
-            Lottie.asset('images/welcome.json', repeat: false, onLoaded: (a) {
-          _controller
-            ..duration = a.duration
-            ..forward();
-        }),
-      ),
     );
   }
 }
---
+++ lib/pages/homepages.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+    _controller.addStatusListener((status) async {
+      if (status == AnimationStatus.completed) {
+        Navigator.pop(context);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Lottiepy"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Lottie.asset(
+              'images/guy.json',
+              height: MediaQuery.of(context).size.height * 0.5,
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: showDialogButton,
+            icon: const Icon(Icons.delivery_dining),
+            label: Text(
+              "Order Now",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: Theme.of(context).textTheme.headline6!.fontSize,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void showDialogButton() => showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) => Dialog(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Lottie.asset('images/check.json', repeat: false,
+                    onLoaded: (composition) {
+                  _controller
+                    ..duration = composition.duration
+                    ..forward();
+                }),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.009,
+                ),
+                Text(
+                  "Enjoy your Order",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: Theme.of(context).textTheme.bodyText2!.fontSize,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.009,
+                ),
+              ],
+            ),
+          ));
+}
